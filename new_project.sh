8d96021aece533725cc2238ef69e363a945feeb7
devse <corp@m.d>
2022-11-10 19:33:20 +0200

    Add "git remote remove origin" and commit after clone

--- new_project.sh
+++ new_project.sh
@@ -3,10 +3,24 @@ PROJECT=$1
 if [ -z $PROJECT ]; then
 	PROJECT="landing"
 fi
-[ -e "$PROJECT" ] && exit -1
+
+[ -e "$PROJECT" ] && echo "Can't create folder '$PROJECT' (Already exists)" && exit -1
+
 git clone --depth=1 https://github.com/coffebar/landing_template.git "$PROJECT"
+
 pushd "$PROJECT"
+
+# remove this script
 rm -f new_project.sh
+# change project name in index.html
 sed -i "s|Landing|${PROJECT}|g" index.html
+# remove template repo from 'remote' in git
+# because don't want to push something into template's repo
+git remote remove origin
+# commit automatic changes
+git commit -a -m "blank template"
+# install gulp related stuff
+# and open editor
 npm install && [ -z $EDITOR ] && ./watch.sh || $EDITOR index.html
+
 popd
