20af2fb33338bc58386a749c0501124ad6887926
devse <corp@m.d>
2022-11-17 14:12:05 +0200

    improve build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp'),
   postcss = require('gulp-postcss'),
   browserSync = require('browser-sync').create(),
-  autoprefixer = require('autoprefixer');
+  autoprefixer = require('autoprefixer'),
+  clean = require('gulp-clean');
 
 const sass = require('gulp-sass')(require('node-sass'));
 const cleanCSS = require('gulp-clean-css');
@@ -36,13 +37,30 @@ function watchChanges() {
   gulp.watch(paths.html.src).on('change', browserSync.reload);
 }
 
+gulp.task('clean', () => {
+  return gulp.src('dist/', {read: false, allowEmpty: true}).pipe(clean());
+});
+
 gulp.task('minify-css', () => {
   return gulp
     .src(paths.styles.dest + '*.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest(paths.styles.dest));
+    .pipe(gulp.dest('dist/' + paths.styles.dest));
+});
+
+gulp.task('copy-to-dist', () => {
+  return gulp
+    .src([
+      paths.html.src,
+      '*.png', '*.ico', '*.svg',
+      '*.webmanifest', 
+      'fonts/**',
+      'img/**',
+      'browserconfig.xml'
+    ], {allowEmpty: true, base: "."})
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('sass', styles);
 gulp.task('serve', gulp.series('sass', watchChanges));
-gulp.task('build', gulp.series('sass', 'minify-css'));
+gulp.task('build', gulp.series('clean', 'sass', 'minify-css', 'copy-to-dist'));
